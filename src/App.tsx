b6113896594558fa868bfa50539a7dd99a601b5e
fps <anthony.nnaemeka.umeh@gmail.com>
2023-09-02 11:35:00 +0100

    😭😭 I finished the necessary parts of the course!

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import UseEffect from "./components/UseEffect"
 import UseState from "./components/UseState"
 import UseContext from "./components/UseContext"
 import UseReducer from "./components/UseReducer"
+import CustomHook from "./components/CustomHook"
 
 export const MyContext = createContext("")
 
 const App = () => {
     return (
         <MyContext.Provider value="This is great!">
-            <div className="App">
+            {/* <div className="App">
                 <h2>Hooks</h2>
                 <UseState />
                 <UseEffect />
             </div>
             <UseContext />
-            <UseReducer/>
+            <UseReducer /> */}
+            <CustomHook />
         </MyContext.Provider>
     )
 }
